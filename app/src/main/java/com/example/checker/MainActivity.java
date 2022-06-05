6e6553e318f53de04cab5db481c33086fac6bdfc
Git user <hlamina@ukr.net>
2022-06-05 17:21:22 +0300

    init commit

---
+++ app/src/main/java/com/example/checker/MainActivity.java
@@ -0,0 +1,176 @@
+package com.example.checker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final int REQUEST_IMAGE_CAPTURE = 1;
+    String employee_id;
+    static public String employee_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        IntentIntegrator intentIntegrator = new IntentIntegrator(this);
+        intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+        intentIntegrator.setOrientationLocked(false);
+        intentIntegrator.initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode != RESULT_OK) return;
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            sendRequest(bitMapToBase64(imageBitmap));
+
+            //ImageView imageView = (ImageView)findViewById(R.id.imageView);
+            //imageView.setImageBitmap(imageBitmap);
+            return;
+        }
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                //Cancelled
+            } else {
+                employee_id = result.getContents();
+                dispatchTakePictureIntent();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        takePictureIntent.putExtra("android.intent.extras.CAMERA_FACING", REQUEST_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        } catch (ActivityNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String bitMapToBase64(@NonNull Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+        byte[] byteArray = byteArrayOutputStream .toByteArray();
+        return Base64.encodeToString(byteArray, Base64.DEFAULT);
+    }
+
+    private void sendRequest(@NonNull String base64) {
+        showFragment(new LoaderFragment());
+        try {
+            JSONObject jsonBody = new JSONObject();
+            jsonBody.put("id", employee_id);
+            jsonBody.put("photo", base64);
+            final String requestBody = jsonBody.toString();
+
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.POST,
+                    "https://app-time-tracking.herokuapp.com/employees/check",
+                    null,
+                    response -> handleResponse(response),
+                    error -> handleError(error)
+            ) {
+                @Override
+                public String getBodyContentType() {
+                    return "application/json; charset=utf-8";
+                }
+
+                @Override
+                public byte[] getBody() {
+                    return requestBody.getBytes(StandardCharsets.UTF_8);
+                }
+            };
+            requestQueue.add(jsonObjectRequest);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleResponse(JSONObject response) {
+        Log.i("Check", response.toString());
+        try {
+            MainActivity.employee_name = response.getString("name");
+            if (response.getBoolean("result")) {
+                showFragment(new SuccessFragment());
+            } else {
+                showFragment(new FailedFragment());
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+            showFragment(new ErrorFragment());
+        }
+        removeAllFragmentsDelay(5000);
+    }
+
+    private void handleError(VolleyError error) {
+        Log.e("Check", error.toString());
+        showFragment(new ErrorFragment());
+        removeAllFragmentsDelay(5000);
+    }
+
+    private void showFragment(Fragment f) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frame_layout, f);
+        ft.addToBackStack(null);
+        ft.commitAllowingStateLoss();
+    }
+
+    private void removeAllFragmentsDelay(int time) {
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(time);
+                for (Fragment fragment : getSupportFragmentManager().getFragments()) {
+                    if (fragment != null) {
+                        getSupportFragmentManager().beginTransaction().remove(fragment).commit();
+                    }
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+}
